187b1d7560ee6d674c366e4876907799cbd7db21
Azzi-Mo <alhwarymostafa@gmail.com>
2023-09-11 12:39:07 +0300

    edit

--- clinet/src/pages/Header.jsx
+++ clinet/src/pages/Header.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 
 const Header = () => {
+
+
   return (
     <header>
       <Link to="/" className="logo">
-        MyBlog
+      Blog
       </Link>
+
       <nav>
         <Link to="/login">login</Link>
         <Link to="/register" className="Register">
